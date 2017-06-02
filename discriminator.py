0ca2265062be4afd32518c58658e6662f8371658
TrsNium <umenyan.hy@gmail.com>
2017-06-02 12:35:15 +0900

    .

--- discriminator.py
+++ discriminator.py
@@ -2,15 +2,10 @@ import tensorflow as tf
 from Unet import UNet
 
 class Discriminator():
-    def __init__(self, image, reuse=False, ini):
-        with tf.variable_scope("discriminator") as scope:
-            if reuse:
-                tf.get_variable_scope().reuse_variables()
-            else:
-                assert tf.get_variable_scope().reuse == False
-            h0 = tf.nn.relu(tf.layers.batch_normalization(tf.layers.conv2d(image, filters=64, kernel_size=[3,3], strides=(2,2), padding='SAME',name=ini+'_h0_conv')))
-            h1 = tf.nn.relu(tf.layers.batch_normalization(tf.layers.conv2d(h0, filters=128, kernel_size=[3,3], strides=(2,2), padding='SAME', name=ini+'_h1_conv')))
-            h2 = tf.nn.relu(tf.layers.batch_normalization(tf.layers.conv2d(h1, filters=256, kernel_size=[3,3], strides=(2,2),padding='SAME', name=ini+'_h2_conv')))
-            h3 = tf.nn.relu(tf.layers.batch_normalization(tf.layers.conv2d(h2, filters=512, kernel_size=[3,3], strides=(2,2),padding='SAME', name=ini+'_h3_conv')))
-            self.last_h = tf.layers.dense(tf.reshape(h3, [-1,32*32*512]), 1, name=ini+'_dense_layer')
-            self.out = tf.nn.sigmoid(self.last_h,name='out')
+    def __init__(self, image, ini):
+        h0 = tf.nn.relu(tf.layers.batch_normalization(tf.layers.conv2d(image, filters=64, kernel_size=[3,3], strides=(2,2), padding='SAME',name=ini+'_h0_conv')))
+        h1 = tf.nn.relu(tf.layers.batch_normalization(tf.layers.conv2d(h0, filters=128, kernel_size=[3,3], strides=(2,2), padding='SAME', name=ini+'_h1_conv')))
+        h2 = tf.nn.relu(tf.layers.batch_normalization(tf.layers.conv2d(h1, filters=256, kernel_size=[3,3], strides=(2,2),padding='SAME', name=ini+'_h2_conv')))
+        h3 = tf.nn.relu(tf.layers.batch_normalization(tf.layers.conv2d(h2, filters=512, kernel_size=[3,3], strides=(2,2),padding='SAME', name=ini+'_h3_conv')))
+        self.last_h = tf.layers.dense(tf.reshape(h3, [-1,32*32*512]), 1, name=ini+'_dense_layer')
+        self.out = tf.nn.sigmoid(self.last_h,name='out')
