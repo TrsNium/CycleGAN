76f109c4cf76c10c21d5e8dee3384d25fdf6a6c5
TrsNium <umenyan.hy@gmail.com>
2017-06-11 00:10:32 +0900

    .

--- util.py
+++ util.py
@@ -1,7 +1,26 @@
 import tensorflow as tf
+from PIL import Image
+import numpy as np
+import random
+import os
 
-def lrelu(x, leak=0.2, name="lrelu"):
-    with tf.variable_scope(name):
-         f1 = 0.5 * (1 + leak)
-         f2 = 0.5 * (1 - leak)
-         return f1 * x + f2 * abs(x)
+def sample(size, channel, path, batch_size):
+    choice_file_names = random.sample(os.listdir(path), batch_size)
+    imgs = np.empty((0,size,size,channel), dtype=np.float32)
+
+    for file_name in choice_file_names:
+        img = np.array(Image.open(path+file_name)).astype(np.float32)
+        imgs = np.append(imgs, np.array([img]), axis=0)
+    imgs = imgs.reshape((-1,size,size,channel))
+    return imgs
+
+def sample_X_Y(size, channel, X_path, Y_path, batch_size):
+    batch_X = sample(size, channel, X_path, batch_size)
+    batch_Y = sample(size, channel, Y_path, batch_size)
+    return batch_X, batch_Y
+
+def visualize(size,  X, fakeX, Y, fakeY, batch_size, i):
+    for n in range(batch_size):
+        img = np.concatenate((X[n], fakeY[n], Y[n], fakeX[n]),axis=1)
+        img = Image.fromarray(np.uint8(img))
+        img.save('./visualized/batch_num{}itr{}.jpg'.format(n,i))
